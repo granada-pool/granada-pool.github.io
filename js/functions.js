2947278ffb16b351b4b380e32c3ec48edb9f3869
VitoMelchionna <vitomelchionn@gmail.com>
2021-07-10 15:01:01 +0200

    test: Facebook chat change

--- js/functions.js
+++ js/functions.js
@@ -2,16 +2,6 @@ $(document).ready(function(){
 
   var $win = $(window);
   var menuWidth = $win.width() <= 800 ? "60%" : "20%";
-  var chatbox = document.getElementById('fb-customer-chat');
-  chatbox.setAttribute("page_id", "101994408826232");
-  chatbox.setAttribute("attribution", "page_inbox");
-
-  $win.fbAsyncInit = function() {
-    FB.init({
-      xfbml            : true,
-      version          : 'v11.0'
-    });
-  };
 
   $(function() {
     $('a[href*="#"]:not([href="#"])').click(function() {
@@ -128,12 +118,3 @@ $(document).ready(function(){
     });
   }
 });
-
-//FACEBOOK LIVE CHAT
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
