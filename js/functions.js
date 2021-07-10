cf0378b10b0da3ae76e979fee69b42e8528df59d
VitoMelchionna <vitomelchionn@gmail.com>
2021-07-10 14:45:36 +0200

    Added Facebook live chat

--- js/functions.js
+++ js/functions.js
@@ -2,6 +2,16 @@ $(document).ready(function(){
 
   var $win = $(window);
   var menuWidth = $win.width() <= 800 ? "60%" : "20%";
+  var chatbox = document.getElementById('fb-customer-chat');
+  chatbox.setAttribute("page_id", "101994408826232");
+  chatbox.setAttribute("attribution", "page_inbox");
+
+  $win.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v11.0'
+    });
+  };
 
   $(function() {
     $('a[href*="#"]:not([href="#"])').click(function() {
@@ -118,3 +128,12 @@ $(document).ready(function(){
     });
   }
 });
+
+//FACEBOOK LIVE CHAT
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
